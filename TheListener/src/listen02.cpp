ab85785949ea4ca05354a9b1b49fc339af5891cf
2838junk <2838junk@gmail.com>
2015-02-17 11:31:32 -0800

    updated before 'demonstrated progress' deadline

--- TheListener/src/listen02.cpp
+++ TheListener/src/listen02.cpp
@@ -1,20 +1,58 @@
-#include <iostream>
+#include<iostream>
+#include<sstream>
+#include<fstream>
+#include<string>
 
 using namespace std;
 
+#define ADC_PATH "/sys/bus/iio/devices/iio:device0/in_voltage"
 
 /* FUNCTIONS */
 
-int readADC(int nAIN){
-	// Argument is analog input number (e.g. "0" for AIN0/P9_39)
-	// Concatenate path and read value
-	// Return single read value of AIN#
+int readAnalog(int number){
+	// Argument is analog input number (e.g. "0" for AIN0/P9_39);
+	// Note: At boot, need to enable BB-ADC in /sys/devices/bone_capemgr.9/slots
+	stringstream ss;
+	ss << ADC_PATH << number << "_raw";		// Concatenate path to correct analog input
+	fstream fs;
+	fs.open(ss.str().c_str(), fstream::in);
+	fs >> number;							// Re-purpose int number as output of AIN
+	fs.close();
+	return number;							// Return single read value of AIN#
 }
 
 int readMic(int nMic){
-	// Argument is microphone number
+	// Argument is microphone number; could be fixed or moving
+	// Note:  This should be converted to a time-based loop of greater than 20 ms duration.  One example used 50 ms.
+	int i;						// increment
+	int samples = 1000;			// number of reads per loop; >> convert to milliseconds?
+	int signalMax = 0;			// max/min values from the series
+	int signalMin = 4095;
+	//char filename;				//just for dumping data to file for analysis & development
+
+	//cout << endl <<  "Starting to read microphone: " << loops << " loops of " << samples << " samples." << endl;
+
+	ofstream datadump;
+	datadump.open ("datadump_x.txt");   //open a file to output the data
+
 	// Read a sample series of values from the analog input using readADC(AIN#)
+	for (i=0;i<samples;i++){
+		int value = readAnalog(nMic);
+		if (value>signalMax){
+			signalMax=value;
+		}
+		if (value<signalMin) {
+			signalMin = value;
+		}
+		datadump << value << endl;
+    }
+
 	// Calculate average (max?) peak-to-peak value of sample set; calculate any other values?
+	int peakToPeak = signalMax - signalMin;
+	datadump << "p-p (" << nMic << ") = " << peakToPeak;
+	datadump.close();
+	return peakToPeak;
+
 	// Return peak-to-peak value
 }
 
@@ -24,6 +62,7 @@ int dataMinder(){
 	// Maintains data arrays;
 	// Calculates basic metrics (short average, long average, etc.)
 	// Returns success/failure;
+	return (1);
 }
 
 int driveMotor(int neworientation){
@@ -31,6 +70,7 @@ int driveMotor(int neworientation){
 	// Determine path (direction, distance, speed(?)
 	// Send commands through GPIO output to move motor
 	// Returns success/failure of command issuance
+	return (1);
 }
 
 int Assessor(){
@@ -41,6 +81,7 @@ int Assessor(){
 	// Determine if the orientation should change
 	// If orientation changing, determine new angle and call driveMotor(neworientation)
 	// Look for start trigger on any fixed mic
+	return (1);
 }
 
 int main(){
@@ -50,5 +91,5 @@ int main(){
 
 	/* MAIN CODE */
 	// May include code for dataMinder and Assessor;  readADC, readMic and driveMotor can be functions
-
+	return (1);
 }
